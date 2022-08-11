0900e08d992fc8a1dc92bece66561f8d83c55ddc
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2022-08-11 13:40:30 +0430

    feat: create very basic ui of login page

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+        <v-row class="d-flex flex-column align-center">
+            <v-col cols="6">
+                <v-text-field
+                    label="phone"
+                    outlined
+                ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                    label="pass"
+                    outlined
+                ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+                <v-btn>
+                    ورود
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
