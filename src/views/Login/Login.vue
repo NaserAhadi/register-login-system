10ac1b2c5e3f33fbb24a4388eb6dceb272b9ad1b
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2022-08-11 14:31:48 +0430

    add routing from login page to retrieval password page

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -12,6 +12,9 @@
                     :label="persianLanguageResource.password"
                     outlined
                 ></v-text-field>
+                <router-link :to="{name:'RetrievalPassword'}">
+                    {{persianLanguageResource.forgotPassword}}
+                </router-link>
             </v-col>
             <v-col cols="6">
                 <v-btn>
@@ -24,11 +27,20 @@
 
 <script>
     import persianLanguageResource from '@/lang/fa-IR.js'
+    // import RetrievalPassword from '@/views/RetrievalPassword/RetrievalPassword'
     export default {
         name: 'Login',
         data(){
             return {
                 persianLanguageResource
+            }
+        },
+        // components:{
+        //     RetrievalPassword
+        // },
+        methods:{
+            navigateToRetrievalPassword(){
+
             }
         }
     }
