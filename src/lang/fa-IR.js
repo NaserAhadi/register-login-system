eac41bbfabb9872cf7913bbe8851535f6cf27343
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2022-08-11 14:30:51 +0430

    feat: create Retrieval Password page

--- src/lang/fa-IR.js
+++ src/lang/fa-IR.js
@@ -1,5 +1,8 @@
 export default{
     login: 'ورود',
     phoneNumber:'شماره همراه',
-    password: 'رمز عبور'
+    password: 'رمز عبور',
+    forgotPassword: 'رمز عبور خود را فرمواش کرده اید؟',
+    RetrievalPassword: 'بازیابی رمز عبور',
+    newPassword: 'رمز عبور جدید'
 }
---
+++ src/views/RetrievalPassword/RetrievalPassword.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        {{persianLanguageResource.RetrievalPassword}}
+        <v-row class="d-flex flex-column align-center">
+            <v-col cols="6">
+                <v-text-field
+                    :label="persianLanguageResource.phoneNumber"
+                    outlined
+                ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                    :label="persianLanguageResource.newPassword"
+                    outlined
+                ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+                <v-btn>
+                    {{persianLanguageResource.login}}
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import persianLanguageResource from '@/lang/fa-IR.js'
+    export default {
+        name: 'RetrievalPassword',
+        data(){
+            return {
+                persianLanguageResource
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
