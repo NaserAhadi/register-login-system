dcbdab5da3f9abe5dca5764ba970caee66c105d9
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2022-08-11 13:47:47 +0430

    feat: use language resource in login

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -3,19 +3,19 @@
         <v-row class="d-flex flex-column align-center">
             <v-col cols="6">
                 <v-text-field
-                    label="phone"
+                    :label="persianLanguageResource.phoneNumber"
                     outlined
                 ></v-text-field>
             </v-col>
             <v-col cols="6">
                 <v-text-field
-                    label="pass"
+                    :label="persianLanguageResource.password"
                     outlined
                 ></v-text-field>
             </v-col>
             <v-col cols="6">
                 <v-btn>
-                    ورود
+                    {{persianLanguageResource.login}}
                 </v-btn>
             </v-col>
         </v-row>
@@ -23,8 +23,14 @@
 </template>
 
 <script>
+    import persianLanguageResource from '@/lang/fa-IR.js'
     export default {
         name: 'Login',
+        data(){
+            return {
+                persianLanguageResource
+            }
+        }
     }
 </script>
 
