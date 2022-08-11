629514f6d7ecce16652847ccf87349ec71f075f6
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2022-08-11 22:40:13 +0430

    feat: register store

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
@@ -11,5 +12,6 @@ Vue.use(VueAxios, axios)
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
