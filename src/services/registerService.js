638fba7fb92aee8fc5a80c93f1afe1b77bfdbc9a
Naser Ahadi <Ahadi.N@orchidpharmed.com>
2022-08-12 18:02:18 +0430

    refactor: change base url

--- src/services/registerService.js
+++ src/services/registerService.js
@@ -2,9 +2,9 @@ import { apiService } from "./apiService"
 
 export default {
     httpCheckRegistering(userPhoneNumberPayload){
-        return apiService.post('register/check/mobile', userPhoneNumberPayload)
+        return apiService.post('v2/register/check/mobile', userPhoneNumberPayload)
     },
     httpVerifyOtp(otpPayload){
-        return apiService.post('/mobile/verify',otpPayload)
+        return apiService.post('v2/mobile/verify',otpPayload)
     },
 }
